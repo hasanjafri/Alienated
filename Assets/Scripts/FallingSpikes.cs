c2a6145a4e4bbec3673775a236d227510507e59f
Hasan <hasan.jafri7@gmail.com>
2018-11-26 15:11:59 -0500

    WIP: Falling Spikes

---
+++ Assets/Scripts/FallingSpikes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingSpikes : MonoBehaviour {
+
+    Rigidbody2D myRigidBody;
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody2D>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+      if (collision.gameObject.tag == "Player")
+      {
+        myRigidBody.isKinematic = false;
+      }
+    }
+}
